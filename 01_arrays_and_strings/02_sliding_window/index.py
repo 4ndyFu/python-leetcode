45f79a1f3c3f78af0db989b8ab7c84ef46932399
4ndyFu <lBYzeMWn@proton.me>
2024-06-28 17:00:11 +0800

    add sliding window

---
+++ 01_arrays_and_strings/02_sliding_window/index.py
@@ -0,0 +1,20 @@
+def maxSum(arr, windowSize):
+    arraySize = len(arr)
+    if (arraySize <= windowSize):
+        print("Invalid operation")
+        return -1
+    
+    windowSum = sum([arr[i] for i in range(windowSize)])
+    maxSum = windowSum
+
+    for i in range(arraySize - windowSize):
+        windowSum = windowSum - arr[i] + arr[i + windowSize]
+        maxSum = max(windowSum, maxSum)
+
+    return maxSum
+
+
+arr = [80, -50, 90, 100]
+k = 2
+answer = maxSum(arr, k)
+print(answer)
